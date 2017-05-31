e732610b150609f61782f39a6e9360e8e65e38fa
Liam <15lcobb@stjosephsrush.ie>
2017-05-31 21:53:15 +0100

    Update Py.py

--- Py.py
+++ Py.py
@@ -1,18 +1,20 @@
-from time import sleep
-x=("1000")	
-hi = ["pistol", "shotgun"]
-def ar():
-    hi.append("assault rifle")
+from time import sleep #allows sleep to be used
+x=("1000") #this is the amount of money the player has
+
+hi = ["pistol", "shotgun"] this is what the player has 
+
+def ar():    #defs allow pieces of code to be inserted into the code using just a word
+    hi.append("assault rifle")    #adds assault rifle to the player
     print("you now have")
     print(hi)
-    sleep(3)
-    x1=int(x)-600
+    sleep(3)    #this stops the programme for 3 seconds
+    x1=int(x)-600    #this changes the amount of money the player has
     print("your balance is now")
     print(x1)
     
     
 def s():
-    hi.append("sniper")
+    hi.append("sniper")    #adds a sniper to the character
     print("you now have")
     print(hi)
     sleep(3)
@@ -21,31 +23,33 @@ def s():
     print(x2)
     
 def pa():
-    hi.append("pistol ammo")
+    hi.append("pistol ammo") 
     print("you now have")
     print(hi)
     skeep(3)
-    x3=int(x)-100
+    x3=int(x)-100 #adds pistol ammo to the character
     print("your balance is now")
     print(x3)
     
 def stuff():
     print("what would you like to buy?")
 
-    trader=input("1)assault rifle ,2)sniper ,3)pistol ammo")
+    trader=input("1)assault rifle ,2)sniper ,3)pistol ammo")    #inputs allow users of the code to chooses an option
     if trader=="1":
-        ar()
+        ar()    #uses def ar():
 	
 	
     elif trader=="2":
-        s()
+        s()    #uses def s():
 
     elif trader=="3":
-        pa()
+        pa()    #uses def pa():
 
     else:
-        print(" we dont sell" + buy_choice + " sorry")
-
+        print("the question was simple")
+	sleep(2)
+	print("just type 1,2 or 3")
+	
 def kill():
     print("you now have")
     hi.append("sniper")
@@ -91,6 +95,7 @@ elif z=="2":
 	
 else:
     print("you are supposed to choose either number 1 OR 2")
+    sleep(2)
     print("it's quite simple really")
 
 
